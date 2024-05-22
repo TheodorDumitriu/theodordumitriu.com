bbcd2473f0f73026496f5e380dab27f61cba4630
Theodor Dumitriu <mihaidumitriu@theodors-macbook-pro.home>
2024-05-22 11:50:47 +0200

    WIP

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import "./Header.css";
+
+export default function Header() {
+  return (
+    <header className="header">
+      <div className="container">
+        <span className="header__title">Theodor Dumitriu</span>
+        <p>Full-Stack Developer </p>
+      </div>
+    </header>
+  );
+}
---
+++ src/components/Hero.jsx
@@ -0,0 +1,27 @@
+import "./Hero.css";
+
+import MugShot from "../assets/theodor-dumitriu.jpg";
+
+export default function Hero() {
+  return (
+    <>
+      <div className="hero container d-flex align-items-center justify-content-space-between">
+        <div className="hero__caption-container">
+          <h1>Me... in a few wise words</h1>
+          <p className="hero__caption">
+            Passionate about technology and the web, I am constantly exploring
+            the frontiers of new innovations. Over the past six years, I have
+            thrived as both a Full Stack Developer and a Tech Lead within a
+            dynamic digital agency. In this role, I orchestrated cohesive teams
+            of developers, UX/UI designers, consultants. Now, I am on the
+            lookout for new challenges and opportunities within the
+            ever-evolving realm of the tech world.
+          </p>
+        </div>
+        <div className="hero__mugshot-container">
+          <img src={MugShot} alt="Theodor Dumitriu" />
+        </div>
+      </div>
+    </>
+  );
+}
